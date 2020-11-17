ccfa774d55a4f784543d85b99b1e99e0cec2fba6
James Bradley <github@jbrew.co.uk>
2020-11-17 22:22:39 +0000

    rm defimpl Jason.Encoder for Association.NotLoaded

--- lib/ecto/json.ex
+++ lib/ecto/json.ex
@@ -1,26 +1,4 @@
 if Code.ensure_loaded?(Jason.Encoder) do
-  defimpl Jason.Encoder, for: Ecto.Association.NotLoaded do
-    def encode(%{__owner__: owner, __field__: field}, _) do
-      raise """
-      cannot encode association #{inspect(field)} from #{inspect(owner)} to \
-      JSON because the association was not loaded.
-
-      You can either preload the association:
-
-          Repo.preload(#{inspect(owner)}, #{inspect(field)})
-
-      Or choose to not encode the association when converting the struct \
-      to JSON by explicitly listing the JSON fields in your schema:
-
-          defmodule #{inspect(owner)} do
-            # ...
-
-            @derive {Jason.Encoder, only: [:name, :title, ...]}
-            schema ... do
-      """
-    end
-  end
-
   defimpl Jason.Encoder, for: Ecto.Schema.Metadata do
     def encode(%{schema: schema}, _) do
       raise """
